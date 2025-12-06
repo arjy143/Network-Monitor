607dedf9a643a2dec82e91750d6e3283dea3236c
Arjy143 <arjun_p_uk@yahoo.co.uk>
2025-12-06 12:05:18 +0000

    basic ui layout

--- design log/design log.md
+++ design log/design log.md
@@ -1,9 +1,13 @@
 # 6/12/25
 
 DONE:
-- I now have a basic UI that shows the interfaces
+- I now have a basic UI that shows the interfaces, and a basic layout
 
 TODO:
 - plan out the various pages. I'm thinking of a side bar that has selectable devices/interfaces, then the main section should have a continuously running list of packets, and maybe some graphs of how much traffic is going where.
 
 ![alt text](image.png)
+
+- an idea of what the ui might be like:
+
+![alt text](image-1.png)
--- src/main.cpp
+++ src/main.cpp
@@ -20,32 +20,41 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    std::string device_list = "Interfaces:\n";
+    std::string device_list;
 
     for (pcap_if_t* device = all_devs; device != nullptr; device = device->next)
     {
-        device_list += " - ";
         device_list += device->name;
-        device_list += "\n";
+        device_list += " | ";
     }
 
     pcap_freealldevs(all_devs);
 
-    int height = 10;
-    int width = 20;
-    int start_y = 10;
-    int start_x = 10;
+    int max_y, max_x;
+    getmaxyx(stdscr, max_y, max_x);
 
-    WINDOW* win = newwin(height, width, start_y, start_x);
+    WINDOW* interface_bar  = newwin(3, max_x, 0, 0);
+    WINDOW* status_bar = newwin(3, max_x, max_y - 3, 0);
+    WINDOW* main_win = newwin(max_y - 6, max_x, 3, 0);
     //refresh to let application know new window exists
     refresh();
+    mvwprintw(interface_bar, 1, 1, "%s", device_list.c_str());
+    mvwprintw(status_bar, 1, 1, "Press F1-F4 to switch panels");
+    mvwprintw(main_win, 1, 2, "Hello from the main panel!");
 
     //put content before boxing
-    wprintw(win, "%s", device_list.c_str());
+    //wprintw(win, "%s", device_list.c_str());
 
-    box(win, 0,0);
-    wrefresh(win);
+    //box(win, 0,0);
+    //wrefresh(win);
 
+    box(interface_bar,0,0);
+    box(status_bar,0,0);
+    box(main_win,0,0);
+
+    wrefresh(interface_bar);
+    wrefresh(status_bar);
+    wrefresh(main_win);
     //ui.print_center(device_list.c_str());
     ui.refresh();
     ui.wait_for_key();
